e60553367cbae09a8e8704b43384837a74581273
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-15 15:42:05 -0400

    Added dotenv

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const fs = require("fs")
+require("dotenv").config()
 app.use(express.json())
 
 app.listen(process.env.PORT || 3000, () => {
