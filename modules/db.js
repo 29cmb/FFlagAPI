1342f200226666c8b037d89fe6b3af30864a0fab
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-15 15:41:33 -0400

    Database setup

---
+++ modules/db.js
@@ -0,0 +1,32 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DATABASEUSER}:${process.env.DATABASEPASS}@${process.env.DATABASEURI}/?retryWrites=true&w=majority&appName=${process.env.DATABASEAPPNAME}`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+module.exports = {
+    client: client,
+    databases: {},
+    collections: {},
+    async run() {
+        try {
+            await client.connect();
+
+            // assign databases
+            const db = client.db(process.env.DATABASENAME)
+            this.databases.main = db
+            this.collections.fflags = db.collection(process.env.FFlagsCollectionName)
+
+            // ping
+            await db.command({ ping: 1 });
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        } finally {
+            await client.close();
+        }
+    },
+}
