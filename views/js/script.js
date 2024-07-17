a331f3db13123e6a59ea087b27c396b3f7058c24
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-17 11:40:15 -0400

    Base for edit mode

--- views/js/script.js
+++ views/js/script.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(data => createTable(data.data))
         .catch(error => console.error('Error fetching data:', error));
+        
+    document.getElementById('editButton').addEventListener('click', () => {
+        toggleEditMode();
+    });
 });
 
 function createTable(data) {
@@ -20,4 +24,42 @@ function createTable(data) {
 
         tbody.appendChild(row)
     });
+}
+
+function toggleEditMode() {
+    const editButton = document.getElementById('editButton');
+    const isEditing = editButton.classList.toggle('editing');
+    const cells = document.querySelectorAll('#dashboard tbody td');
+
+    cells.forEach(cell => {
+        if(isEditing){
+            if(cell.textContent == "✅") cell.innerHTML = "true"
+            if(cell.textContent == "❌") cell.innerHTML = "false"
+        } else {
+            if(cell.textContent == "true") cell.innerHTML = "✅"
+            if(cell.textContent == "false") cell.innerHTML = "❌"
+        }
+        
+        cell.setAttribute('contenteditable', isEditing);
+    });
+
+    if (isEditing) {
+        editButton.innerHTML = '<i class="fas fa-save"></i>';
+    } else {
+        editButton.innerHTML = '<i class="fas fa-pencil-alt"></i>';
+        saveChanges();
+    }
+}
+
+function saveChanges() {
+    const rows = document.querySelectorAll('#dashboard tbody tr');
+    const data = [];
+
+    rows.forEach(row => {
+        const flag = row.cells[0].textContent.trim();
+        const value = row.cells[1].textContent.trim();
+        data.push({ flag, value });
+    });
+
+    // handle the saving
 }
