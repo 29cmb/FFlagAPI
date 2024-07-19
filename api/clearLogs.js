70605df1d3aa5d3c7463900bd9f8206152b0c58c
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-19 16:52:43 -0400

    Actually connect to the API

--- api/clearLogs.js
+++ api/clearLogs.js
@@ -1,8 +1,10 @@
 const db = require("../modules/db.js")
 module.exports = (app) => {
     app.post("/api/clearLogs", async (req, res) => {
+        await db.client.connect()
         const result = await db.collections.logs.deleteMany({})
         res.json({ success: true, message: `Deleted ${result.deletedCount} logs` })
+        await db.client.close()
     })
 
     return {
